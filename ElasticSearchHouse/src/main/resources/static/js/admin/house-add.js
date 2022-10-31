26ca3b9679d2b5b170c625c62f787cff197bf47e
xichuanwushao <avatarwx@163.com>
2022-10-31 09:58:01 +0800

    取消封面配置

--- ElasticSearchHouse/src/main/resources/static/js/admin/house-add.js
+++ ElasticSearchHouse/src/main/resources/static/js/admin/house-add.js
@@ -118,11 +118,11 @@ $(function () {
         submitHandler: function (form) {
             var cover = $(form).find("input:radio[name='cover']:checked").val();
 
-            if (cover == null || typeof(cover) == "undefined" || cover == "" || cover.length < 1) {
-                layer.msg('至少要上传一个封面！', {icon: 5, time: 2000});
-                return false;
-            }
-
+            // if (cover == null || typeof(cover) == "undefined" || cover == "" || cover.length < 1) {
+            //     layer.msg('至少要上传一个封面！', {icon: 5, time: 2000});
+            //     return false;
+            // }
+            var cover = "http://192.168.170.1:65535/tupian/400_ChwFlGJXwlKAaJIvAANecRd4bcg240.jpg";
             $(form).find('input.house-tag').remove();
             var index = 0;
             tags.forEach(function (tag) {
